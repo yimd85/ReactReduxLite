d667668a7b4eea1daa4394c5e12bb00f2698f435
David Yim <yimd85@gmail.com>
2018-09-14 13:26:57 -0400

    added action, added linter

---
+++ Blog/src/components/post_index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class PostsIndex extends Component {
+    render() {
+        return (
+            <div>
+                Post Index
+            </div>
+        );
+    }
+}
+
+
+export default PostsIndex;
--- Blog/src/index.js
+++ Blog/src/index.js
@@ -3,29 +3,32 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
+import promise from 'redux-promise';
 
-import App from './components/app';
+
+// import App from './components/app';
+import PostsIndex from './components/post_index';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
-class Hello extends React.Component{
-  render(){return <div>Hello</div>}
-}
+// class Hello extends React.Component{
+//   render(){return <div>Hello</div>}
+// }
 
-class Goodbye extends React.Component{
-  render(){return <div>Goodbye</div>}
-}
+// class Goodbye extends React.Component{
+//   render(){return <div>Goodbye</div>}
+// }
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     {/* <App /> */}
     <BrowserRouter>
       <div>
-        <Route path='/hello' component={Hello} />
-        <Route path='/goodbye' component={Goodbye} />
+        {/* <Route path='/hello' component={Hello} />
+        <Route path='/goodbye' component={Goodbye} /> */}
+        <Route path='/' component={PostsIndex} />
       </div>
-
     </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
